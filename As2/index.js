3f91e6246454e26da31706307812d1352f56e664
Karbhari Gadekar <karbharigadekar706@gmail.com>
2023-02-16 23:37:30 +0530

    Assignment 02

---
+++ As2/index.js
@@ -0,0 +1,12 @@
+const containt = <div className="content">
+    <h1>The Evolution of Airbnb's Frontend</h1>
+    <div className="Logo">
+        <p className="sp">@ Spikebrehum</p>
+        <div class="logo">
+            <i class="fa-brands fa-airbnb"></i>
+            <p>Airbnb</p>
+        </div>
+    </div>
+</div>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(containt)
